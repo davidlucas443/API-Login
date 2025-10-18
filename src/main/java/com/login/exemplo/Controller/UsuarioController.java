f0a1454064267ac90054d1bc597371b5cf2847ce
davidlucas443 <davidga19391@gmail.com>
2025-10-18 08:20:31 -0300

    feat: create login

---
+++ src/main/java/com/login/exemplo/Controller/UsuarioController.java
@@ -0,0 +1,39 @@
+package com.login.exemplo.Controller;
+
+import com.login.exemplo.Entity.Usuario;
+import com.login.exemplo.Repostories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioController {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @PostMapping(value = "usuario/cadastro")
+    public ResponseEntity<Usuario> saveUser(@RequestBody Usuario user) {
+        Usuario usuario = new Usuario(user.getName(), user.getEmail(), user.getPassword());
+        usuarioRepository.save(usuario);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<?> findUser(@RequestBody Usuario user){
+        Usuario findUser = usuarioRepository.findByEmail(user.getEmail());
+        if (findUser == null){
+            return ResponseEntity.ok("Usuário não encontrado");
+        }
+        else {
+            if (findUser.getPassword().equals(user.getPassword())) {
+                return ResponseEntity.ok("Logado com sucesso");
+            }
+            else{
+                return ResponseEntity.ok("Senha incorreta");
+            }
+        }
+    }
+}
